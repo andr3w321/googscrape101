1b06523b43ed7cd742c1b9bf503ef45513379c60
Andrew Rennhack <arennhack@gmail.com>
2012-01-22 22:53:31 -0800

    reset to basic auth

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
-  #before_filter :authenticate
+  before_filter :authenticate
 
   def search
   end
--- config/environments/staging.rb
+++
@@ -1,5 +0,0 @@
-MyApp::Application.configure do
-  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
-    [u, p] == ['user', 'pass']
-  end
-end
