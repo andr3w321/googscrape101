15fdf8c284d61e4ee138f5d62776f4968c8d21c7
Andrew Rennhack <arennhack@gmail.com>
2012-01-26 15:46:14 -0800

    fixed news,images,results still working on activeadmin view

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -13,19 +13,16 @@ module SearchHelper
     if(!params[:q].nil?)
       doc = Nokogiri::HTML(open("http://www.google.com/search?q=#{CGI.escape(params[:q])}&num=#{CGI.escape(params[:number_of_results])}&pws=0"))
       doc.css(".g").each do |node|
-        #to filter out images & video results uncomment section and delete below
-        #if !node.at_css(".s").nil?
-          #href       = node.at_css("h3").at_css("a")[:href]
-          #href_text  = node.at_css("h3").text
+        #filter out images, news & video results which are links to additional google searches
+        if !node.at_css(".s").nil?
+          href       = node.at_css("h3").at_css("a")[:href]
+          href_text  = node.at_css("h3").text
+          @linkslist << href << "\r\n"
+          results << Result.link_only(href, href_text)
           #green_link = node.at_css("cite").text
           #summary    = node.at_css(".s").text
           #results << Result.new(href, href_text, green_link, summary)
-        #end
-        
-        href       = node.at_css("h3").at_css("a")[:href]
-        href_text  = node.at_css("h3").text
-        @linkslist << href << "\r\n"
-        results << Result.link_only(href, href_text)
+        end
       end
     end
     results
